28f2f6ecdb4d14d087d23e85fa3069b2b40beaa6
TugceKanlioglu <t.kanlioglu@campus.lmu.de>
2026-01-12 17:19:08 +0100

    Setup RAF-DB pretraining and FER finetuning pipeline (64px)

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,25 +1,54 @@
 import torch
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, Subset
 from torchvision import datasets
+from sklearn.model_selection import train_test_split
 
-from .transforms import fer_train_transforms, fer_eval_transforms
+from .transforms import (
+      raf_train_transforms, raf_eval_transforms,
+      fer_train_transforms, fer_eval_transforms
+)
 
-def make_fer_loaders(data_path, batch_size=64):
-    full_train_dataset = datasets.ImageFolder(
-        root=f"{data_path}/train", 
-        transform=fer_train_transforms()
-    )
-    train_size = int(0.9 * len(full_train_dataset))
-    val_size = len(full_train_dataset) - train_size
-
-    train_dataset, val_dataset = random_split(
-        full_train_dataset, 
-        [train_size, val_size],
-        generator=torch.Generator().manual_seed(42)
+def make_raf_loaders(data_path, batch_size=64):
+    train_data = datasets.ImageFolder(root=f"{data_path}/train", transform=raf_train_transforms())
+    val_data = datasets.ImageFolder(root=f"{data_path}/train", transform=raf_eval_transforms())
+   
+    train_idx, val_idx = train_test_split(
+          range(len(train_data)),
+          test_size=0.1,
+          stratify=train_data.targets,
+          random_state=42
     )
 
     train_loader = DataLoader(
-        train_dataset,
+        Subset(train_data, train_idx),
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=4
+    )
+     
+    val_loader = DataLoader(
+        Subset(val_data, val_idx),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=4
+    )
+    return train_loader, val_loader
+
+
+def make_fer_loaders(data_path, batch_size=64):
+    train_data = datasets.ImageFolder(root=f"{data_path}/train", transform=fer_train_transforms())
+    val_data = datasets.ImageFolder(root=f"{data_path}/train", transform=fer_eval_transforms())
+
+    train_idx, val_idx = train_test_split(
+          range(len(train_data)),
+          test_size=0.1,
+          stratify=train_data.targets,
+          random_state=42
+    )
+
+    train_loader = DataLoader(
+        Subset(train_data, train_idx),
         batch_size=batch_size,
         shuffle=True,
         drop_last=True,
@@ -27,7 +56,7 @@ def make_fer_loaders(data_path, batch_size=64):
     )
     
     val_loader = DataLoader(
-        val_dataset,
+        Subset(val_data, val_idx),
         batch_size=batch_size,
         shuffle=False,
         num_workers=4
@@ -36,7 +65,7 @@ def make_fer_loaders(data_path, batch_size=64):
     return train_loader, val_loader
   
 if __name__ == "__main__":
-        train_loader, val_loader = make_fer_loaders("data/FER13")
+        train_loader, val_loader = make_raf_loaders("data/DATASET")
         images, labels = next(iter(train_loader))
         print(f"Batch-Shape: {images.shape}")
 
--- src/data/transforms.py
+++ src/data/transforms.py
@@ -1,5 +1,23 @@
 from torchvision import transforms
 
+def raf_train_transforms():
+    return transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+     ])  
+
+def raf_eval_transforms():
+    return transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+
 def fer_train_transforms():
     return transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
@@ -7,8 +25,7 @@ def fer_train_transforms():
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomRotation(10),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5], std=[0.5]),    
-
+        transforms.Normalize(mean=[0.5], std=[0.5])    
     ])
 
 def fer_eval_transforms():
