2856e66da8d3972424e2e28487042def37481083
Kris Arvind Muthukumar <k.muthukumar@campus.lmu.de>
2026-01-13 16:35:24 +0100

    First Model: Training with FER13

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -3,12 +3,11 @@ from torchvision import transforms
 def fer_train_transforms():
     return transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
-        transforms.Resize((64,64)),
+        transforms.RandomResizedCrop(64, scale=(0.9, 1.0)),
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomRotation(10),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5], std=[0.5]),    
-
+        transforms.Normalize(mean=[0.5], std=[0.5]),
     ])
 
 def fer_eval_transforms():
